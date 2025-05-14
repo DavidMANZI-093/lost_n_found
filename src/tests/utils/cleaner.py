22157355395818c040eb7b2ac79695b4c42d2593
DavidMANZI-093 <manzidavid093@gmail.com>
2025-05-14 11:51:06 +0200

    Auto-commit at 14/05/2025, 11:51:05 for inactivity period...

--- src/tests/utils/cleaner.py
+++
@@ -1,7 +0,0 @@
-import typing
-
-def Cleaner(cursor: typing.Any):
-    cursor.execute("DELETE FROM lost_n_found.lost_item")
-    cursor.execute("DELETE FROM lost_n_found.found_item")
-    cursor.execute("DELETE FROM lost_n_found.user")
-    cursor.close()
