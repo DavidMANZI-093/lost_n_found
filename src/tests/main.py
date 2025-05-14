98a2796e1b2b26675547be766f1c415580229bfc
DavidMANZI-093 <manzidavid093@gmail.com>
2025-05-14 09:40:37 +0200

    Auto-commit at 14/05/2025, 09:40:36 for inactivity period...

--- src/tests/main.py
+++ src/tests/main.py
@@ -2,7 +2,19 @@ def __main__(self):
     self.cursor = PSQLConn().connect().cursor()
     self.base_url = "http://localhost:8080/api/v1"
 
-    
+    requests.post(
+        f"{self.base_url}/auth/signup",
+        json={
+            "email": "adminlostnfound@gmail.com",
+            "password": "admin123",
+            "firstName": "CIZA",
+            "lastName": "Justesse",
+            "phoneNumber": "0791111111",
+            "address": "KK 508 St, Kigali"
+        }
+    )
+
+    Cleaner(self.cursor)
 
 if __name__ == "__main__":
     __main__()
