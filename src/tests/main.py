eb1cbc09ca2d37288b014a4c98ae996ff9b18a1c
DavidMANZI-093 <manzidavid093@gmail.com>
2025-05-14 09:27:53 +0200

    Auto-commit at 14/05/2025, 09:27:53 for inactivity period...

--- src/tests/main.py
+++ src/tests/main.py
@@ -1,5 +1,8 @@
-def __main__():
-    cursor = PSQLConn().connect().cursor()
+def __main__(self):
+    self.cursor = PSQLConn().connect().cursor()
+    self.base_url = "http://localhost:8080/api/v1"
+
+    
 
 if __name__ == "__main__":
     __main__()
